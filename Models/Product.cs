2e782b343e96a73b64cb7729fca32fe7e83d2553
Alejandro <you@example.com>
2025-05-08 19:18:30 -0500

    prueba

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // Llave primaria
+        public string name { get; set; }
+        [Column (TypeName ="decimal(6,2")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; } //  Llave foranea  
+        public Category Category { get; set; } // Propiedad de navegacion 
+    }
+}
